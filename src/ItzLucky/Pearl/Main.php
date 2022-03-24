74478e7124ad023f56713ebe8dfe200a3683c858
ItzLucky <64375551+NightDevil9440@users.noreply.github.com>
2022-03-25 01:35:40 +0530

    Update Main.php
    Format the code

--- src/ItzLucky/Pearl/Main.php
+++ src/ItzLucky/Pearl/Main.php
@@ -11,16 +11,16 @@ use pocketmine\entity\projectile\EnderPearl;
 class Main implements Listener
 {
 
-public function onProjectileHit(ProjectileHitEvent) {
-$projectile = $event->getEntity();
-$entity = $projectile->getOwningEntity();
-if($projectile instanceof EnderPearl and $entity instanceof Player){
-    $setPosition = new \ReflectionMethod($entity, 'setPosition');
-    $setPosition->setAccessible(true);
-    $setPosition->invoke($entity, $event->getRayTraceResult()->getHitVector());
-    $location = $entity->getLocation();
-    $entity->getNetworkSession()->syncMovement($location, $location->yaw, $location->pitch);
-    $projectile->setOwningEntity(null);
-}
-}
+    public function onProjectileHit(ProjectileHitEvent) {
+        $projectile = $event->getEntity();
+        $entity = $projectile->getOwningEntity();
+        if ($projectile instanceof EnderPearl and $entity instanceof Player) {
+            $setPosition = new \ReflectionMethod($entity, 'setPosition');
+            $setPosition->setAccessible(true);
+            $setPosition->invoke($entity, $event->getRayTraceResult()->getHitVector());
+            $location = $entity->getLocation();
+            $entity->getNetworkSession()->syncMovement($location, $location->yaw, $location->pitch);
+            $projectile->setOwningEntity(null);
+        }
+    }
 }
