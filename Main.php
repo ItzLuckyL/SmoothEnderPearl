aafc83d7273c0e199537e90698f0b5758a4685a3
ItzLucky <64375551+NightDevil9440@users.noreply.github.com>
2022-03-25 01:22:02 +0530

    Create Main.php

---
+++ Main.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ItzLucky\Pearl;
+
+use pocketmine\player\Player;
+
+use pocketmine\event\entity\ProjectileHitEvent;
+
+use pocketmine\entity\projectile\EnderPearl;
+
+class Main implements Listener
+{
+
+public function onProjectileHit(ProjectileHitEvent) {
+$projectile = $event->getEntity();
+$entity = $projectile->getOwningEntity();
+if($projectile instanceof EnderPearl and $entity instanceof Player){
+    $setPosition = new \ReflectionMethod($entity, 'setPosition');
+    $setPosition->setAccessible(true);
+    $setPosition->invoke($entity, $event->getRayTraceResult()->getHitVector());
+    $location = $entity->getLocation();
+    $entity->getNetworkSession()->syncMovement($location, $location->yaw, $location->pitch);
+    $projectile->setOwningEntity(null);
+}
+}
+}
